2c2f7b318b324e1cd151a95b5ec84a2edfe5fada
gru97 <fatemeh.shahidani@gmail.com>
2020-06-26 12:49:03 +0430

    logging added to service

--- AutoPublisher/ScriptRunner.cs
+++ AutoPublisher/ScriptRunner.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace AutoPublisher
 {
@@ -16,19 +17,31 @@ namespace AutoPublisher
     public class ScriptRunner: IScriptRunner
     {
         private Configuration _configuration;
-        public ScriptRunner(Configuration configuration)
+        private ILogger<ScriptRunner> _logger;
+        public ScriptRunner(Configuration configuration, ILogger<ScriptRunner> logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
         public void PublishService()
         {
             var scriptPath = _configuration.ScriptPath;
+            _logger.LogInformation(scriptPath);
             string script = File.ReadAllText(scriptPath);
+            _logger.LogInformation(script);
             using (var powerShell = PowerShell.Create())
             {
+                _logger.LogInformation("------------beginning to run script-----------------");
                 powerShell.AddScript(script);
-                powerShell.Invoke();
+                var results=powerShell.Invoke();
+                foreach (var psObject in results)
+                {
+                    //System.Diagnostics.Debug.Write(psObject.BaseObject.ToString());
+                    _logger.LogInformation(psObject.BaseObject.ToString());
+                }
+
             }
+            _logger.LogInformation("--------------end of command----------------");
 
         }
     }
